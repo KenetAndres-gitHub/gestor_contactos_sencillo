066bb7edb04c8eaa6023e768f8a605f6ece1fa80
Kenet Laptop Ubuntu <kenbok2018@gmail.com>
2024-11-20 08:32:49 -0500

    Inicio actualizado, con información de la persona

---
+++ migrations/Version20241120131523.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241120131523 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE persona (id SERIAL NOT NULL, nombres VARCHAR(100) NOT NULL, apellidos VARCHAR(100) NOT NULL, sexo VARCHAR(15) NOT NULL, edad INT NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('DROP TABLE persona');
+    }
+}
---
+++ src/Entity/Persona.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonaRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonaRepository::class)]
+class Persona
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $nombres = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $apellidos = null;
+
+    #[ORM\Column(length: 15)]
+    private ?string $sexo = null;
+
+    #[ORM\Column]
+    private ?int $edad = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombres(): ?string
+    {
+        return $this->nombres;
+    }
+
+    public function setNombres(string $nombres): static
+    {
+        $this->nombres = $nombres;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(string $apellidos): static
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getSexo(): ?string
+    {
+        return $this->sexo;
+    }
+
+    public function setSexo(string $sexo): static
+    {
+        $this->sexo = $sexo;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): static
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+}
