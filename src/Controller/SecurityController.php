efcad5a1f6e832bf3430428c37efeebb53b3b27e
Kenet Laptop Ubuntu <kenbok2018@gmail.com>
2024-11-28 14:03:10 -0500

    mailer configurado

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,19 +4,33 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, MailerInterface $mailer): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+
+        $email = (new Email())
+            ->from('kenbok2018@gmail.com')
+            ->to('kenbok2014@hotmail.com')
+            //->cc('cc@example.com')
+            //->bcc('bcc@example.com')
+            //->replyTo('fabien@example.com')
+            //->priority(Email::PRIORITY_HIGH)
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+        $mailer->send($email);
 
         return $this->render('security/login.html.twig', [
             'last_username' => $lastUsername,
